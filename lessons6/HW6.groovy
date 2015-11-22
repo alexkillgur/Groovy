939db298c39ef78427cbf40d9af2bb3fdd358316
Alexey Shashlyuk <alexkillgur@gmail.com>
2015-11-22 21:56:21 +0200

    version 1 of HomeWork 6 (begin)

---
+++ lessons6/HW6.groovy
@@ -0,0 +1,106 @@
+/**
+ * Created by Killgur on 22.11.2015.
+ */
+import groovy.transform.TypeChecked
+import groovy.transform.TypeCheckingMode
+import groovy.transform.CompileStatic
+
+// Difference between @TypeChecked and @CompileStatic
+//-----------------------------------------------------------
+@TypeChecked
+class Computer {
+    int compute( String str ) {
+        str.length()
+    }
+    String compute( int x ) {
+        String.valueOf( x )
+    }
+}
+
+@TypeChecked
+void testTC() {
+    def computer = new Computer()
+    computer.with {
+        assert compute( compute( 'foobar' ) ) =='6'
+    }
+}
+
+@CompileStatic
+void testCS() {
+    def computer = new Computer()
+    computer.with {
+        assert compute( compute( 'foobar' ) ) =='6'
+    }
+}
+
+testTC() // Pass
+Computer.metaClass.compute = { String str -> new Date() } // Monkey patching
+//testTC() // Fail
+testCS() // Pass
+//-----------------------------------------------------------
+
+// Optional typing
+//-----------------------------------------------------------
+String aString = 'foo'
+assert aString.toUpperCase()
+
+def bString = 'foo'
+assert bString.toUpperCase()
+
+private concat( a, b ) {
+    a+b
+}
+
+assert concat( 'foo', 'bar' ) == 'foobar'
+assert concat( 1, 2 ) == 3
+//-----------------------------------------------------------
+
+// TypeChecked annotation
+//-----------------------------------------------------------
+class SentenceBuilder {
+    def storage = []
+
+    def propertyMissing( String name ) { storage << name }
+
+    String toString() {
+        storage.join(' ')
+    }
+}
+
+@TypeChecked
+class GreetingService {
+    String greeting() {
+        doGreet()
+    }
+
+    @TypeChecked( TypeCheckingMode.SKIP )
+    private String doGreet() {
+        def b = new SentenceBuilder()
+        b.with {
+            Hello
+            my
+            name
+            is
+            John
+        }
+        b
+    }
+}
+
+def s = new GreetingService()
+assert s.greeting() == 'Hello my name is John'
+//-----------------------------------------------------------
+
+// Dynamic Groovy
+//-----------------------------------------------------------
+class Person {
+    String firstName
+    String lastName
+}
+
+Person.metaClass.getFormattedName = { "$delegate.firstName $delegate.lastName" }
+
+def p = new Person( firstName: 'Raymond', lastName: 'Devos' )
+assert p.formattedName == 'Raymond Devos'
+//-----------------------------------------------------------
+
