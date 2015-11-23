98ddd8db8807a5911182972fd8cd992466c806c4
Alexey Shashlyuk <alexkillgur@gmail.com>
2015-11-23 14:39:21 +0200

    version 2 of HomeWork 6 (without Duck)

--- lessons6/HW6.groovy
+++ lessons6/HW6.groovy
@@ -4,6 +4,7 @@
 import groovy.transform.TypeChecked
 import groovy.transform.TypeCheckingMode
 import groovy.transform.CompileStatic
+import groovy.transform.TupleConstructor
 
 // Difference between @TypeChecked and @CompileStatic
 //-----------------------------------------------------------
@@ -93,6 +94,7 @@ assert s.greeting() == 'Hello my name is John'
 
 // Dynamic Groovy
 //-----------------------------------------------------------
+@TupleConstructor
 class Person {
     String firstName
     String lastName
@@ -104,3 +106,122 @@ def p = new Person( firstName: 'Raymond', lastName: 'Devos' )
 assert p.formattedName == 'Raymond Devos'
 //-----------------------------------------------------------
 
+// Type checking assignments with Groovy Truth
+// Проверка примеров из документации
+//-----------------------------------------------------------
+@TypeChecked
+class TypeCheck {
+    String stringDate = new Date() // Date.toString()
+    Boolean boxed = 'some string' // Не пустая строка, Groovy Truth
+    boolean prim = 'some string' // Не пустая строка, Groovy Truth
+    Class clazz = 'java.lang.String' // Принудительное приведение к классу
+    String someString = null // Не примитивный тип
+//    int iNull = null // Примитивный тип, fail
+    int[] intArray = new int[4] // Массив типа int
+//    int[] intArrayWithString = new String[4] // Нельзя присвоить массиву типа int значения типа String, fail
+    int[] intList = [1,2,3] // Список типа int
+//    int[] intListWithDate = [1,2, new Date()] // Нельзя присвоить элементу списка типа int значение типа String, fail
+    AbstractList abstractList = new ArrayList() // AbstractList -> суперкласс для ArrayList
+
+    @TypeChecked( TypeCheckingMode.SKIP )
+    void excludeLinkedList() {
+        LinkedList linkedList = new ArrayList() // LinkedList не является суперклассом для ArrayList, fail without @TypeChecked( TypeCheckingMode.SKIP )
+        linkedList << [ 1, [ name: 'killgur' ], new Date(), [ 'Croovy', 2 ] ]
+        println """LinkedList linkedList = new ArrayList()
+        \rВ отличие от остальных примеров не выпадает при компиляции без аанотации @TypeChecked и работает в рантайме - $linkedList"""
+//        int iNull = null // В данном случае отвалится в рантайме
+//        int[] intArrayWithString = new String[4] // В данном случае отвалится в рантайме
+//        int[] intListWithDate = [1,2, new Date()] // В данном случае отвалится в рантайме
+    }
+
+    List list = new ArrayList() // ArrayList реализует интерфейс List
+    //RandomAccess randomAccess = new LinkedList() // LinkedList не реализует интерфейс RandomAccess, fail
+}
+
+TypeCheck typeCheck = new TypeCheck()
+typeCheck.excludeLinkedList()
+
+// Тут все понятно
+int iZero = 0
+Integer bi = 1
+int x = new Integer(123)
+double d = new Float(5f)
+
+// SAM-type
+Runnable r = { println "\nПривет из SAM-type" }
+r()
+
+interface SAMType {
+    int doSomething()
+}
+
+SAMType sam = { 123 }
+assert sam.doSomething() == 123
+
+abstract class AbstractSAM {
+    int calc() { 2* value() }
+    abstract int value()
+}
+
+AbstractSAM c = { 123 }
+assert c.calc() == 246
+
+// Use of @TupleConstructor, list and map constructor
+Person tupleConstructor = new Person('Ada','Lovelace')
+assert tupleConstructor.formattedName == 'Ada Lovelace'
+
+Person listConstructor = ['Ada','Lovelace']
+assert listConstructor.formattedName == 'Ada Lovelace'
+
+Person mapConstructor = [firstName:'Ada', lastName:'Lovelace']
+assert mapConstructor.formattedName == 'Ada Lovelace'
+
+// Method resolution
+class Duck {
+    void quack() {
+        println 'Quack!'
+    }
+}
+
+class QuackingBird {
+    void quack() {
+        println '\nQuack-Quack!'
+    }
+}
+
+@TypeChecked
+void accept( Duck quacker ) {
+    quacker.quack()
+}
+
+accept( new Duck() )
+//accept( new QuackingBird() )
+
+// Type inference
+def message = 'Welcome to Groovy!'
+assert message.toUpperCase() == 'WELCOME TO GROOVY!'
+//println message.upper() // compile time error
+
+// Variables vs fields in type inference
+@TypeChecked
+class SomeClass {
+    def someUntypedField
+    String someTypedField
+
+    @TypeChecked( TypeCheckingMode.SKIP )
+    void someMethod() {
+        someUntypedField = '123'
+        someUntypedField = someUntypedField.toUpperCase()
+    }
+
+    void someSafeMethod() {
+        someTypedField = '123'
+        someTypedField = someTypedField.toUpperCase()
+    }
+
+    void someMethodUsingLocalVariable() {
+        def localVariable = '123'
+        someUntypedField = localVariable.toUpperCase()
+    }
+}
+//-----------------------------------------------------------
